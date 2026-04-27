cb896bbb3a1b4866ff7f94d18b6c9ef639c31e4b
Ner Karso <nerpkarso@gmail.com>
2026-04-26 21:12:38 -0300

    feat: support config and plugins volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,17 +9,40 @@ else
   MOODLE_URL=${MOODLE_URL}
 fi
 
-if [ ! -d /var/www/html/moodle ]; then
+if [ ! -d moodle ]; then
+  # Download and extract Moodle
   curl -o moodle.zip $MOODLE_URL
   unzip moodle.zip
   rm moodle.zip
+
+  # Move config
   mv config-dist.php moodle/config.php
-  cd moodle
-  composer install --no-dev --classmap-authoritative
-  chown -R www-data:www-data /var/www/html
+
+  # Install dependencies
+  composer install --no-dev --classmap-authoritative --working-dir=moodle
+  
+  # Set permissions
+  chown -R www-data:www-data moodle
+fi
+
+# Copy volume mounted config
+if [ -f config.php ]; then
+  cp config.php moodle/config.php
+
+  # Set permissions
+  chown www-data:www-data moodle/config.php
+fi
+
+# Copy volume mounted local plugins
+if [ -d local ]; then
+  mkdir -p moodle/public/local
+  cp -a local/. moodle/public/local/
+
+  # Set permissions
+  chown -R www-data:www-data moodle/public/local
 fi
 
 # Ensure permissions for moodledata
-chown -R www-data:www-data /var/www/html/moodledata
+chown -R www-data:www-data moodledata
 
 exec "$@"
